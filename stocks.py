dd190b71e51d4e1f0d4fbda872e72cb4f54ce62b
Sergio Albors <sergioalborsjuez@gmail.com>
2025-01-24 19:29:01 +0100

    fixed the excel problem

--- stocks.py
+++ stocks.py
@@ -1,6 +1,24 @@
-import yfinance as yf
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import xgboost as xgd
-# Load the data of a given stock (this case Micron Technology(MU))
-data = pd.read_csv()
+import seaborn as sb
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import chardet
+
+# I needed the following code to know the codification of my document, once this code tells us the specific codification we won't need it anymore
+
+with open("MU DATA","rb") as file:
+    result = chardet.detect(file.read())
+    print(result)
+
+import csv
+with open("MU DATA","r", encoding="ascii") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+
+data = pd.read_csv("MU DATA","r", encoding="ascii")
+print(data)
