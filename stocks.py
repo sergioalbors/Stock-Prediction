9b6f51483ca30ba6be264d8efcf43fcd3ec27fc8
Sergio Albors <sergioalborsjuez@gmail.com>
2025-01-27 21:02:31 +0100

    major update

--- stocks.py
+++ stocks.py
@@ -3,29 +3,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from xgboost import XGBClassifier
-from sklearn import metrics
-
 import warnings
 warnings.filterwarnings('ignore')
 
 import chardet
 
-# I needed the following code to know the codification of my document, once this code tells us the specific codification we won't need it anymore
-#with open("DOGE-USD.csv","rb") as file:
+# I need the following code to know the codification of my document, once this code tells us the specific codification we won't need it anymore#with open("MU DATA.csv","rb") as file:
     #result = chardet.detect(file.read())
     #print(result)
 
 import csv
-with open("DOGE-USD.csv","r", encoding="ascii") as file:
+with open("MU DATA.csv", encoding="ascii") as file:
     reader = csv.reader(file)
     for row in reader:
         print(row)
 
-data = pd.read_csv("DOGE-USD.csv","r", encoding="ascii")
+data = pd.read_csv("MU DATA.csv", encoding="ascii")
 print(data)
+
+arr = np.array([1, 2, 3])
+arr_tuple = tuple(arr)  # Convert ndarray to tuple
+my_set = {arr_tuple}  # Now this works
+print(my_set)
+
+plt.figure(figsize=(15,5))
+plt.plot(data)
+plt.title('Micron technology price.', fontsize=20)
+plt.ylabel('price in dollars.')
+plt.show()
+
 
